5ec2420f1b7a6bceb3be4b6e46b1994d35ea9ce2
woodlook786 <woodlook.br@gmail.com>
2024-01-18 20:22:54 -0300

    my-first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
-import ImagesList from './components/ImagesList';
-import UploadForm from './components/UploadForm';
+// import './components/responsive/responsive.css'
+import About from './components/About/About';
+import Blog from './components/Blog/Blog';
+import Contact from './components/Contact/Contact';
+import Feedback from './components/Feedback/Feedback';
+import Nav from './components/Nav/Nav';
+import Projects from './components/Projects/Projects';
+import Services from './components/Services/Services';
+import SiteHeader from './components/SiteHeader/SiteHeader';
+import Responsive from './components/responsive/responsive';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="title">your picture</h1>
-      <UploadForm />
-      <ImagesList />
-    </div>
+    <>
+      <Responsive />
+      <Nav />
+      <SiteHeader />
+      <Projects />
+      <Services />
+      <Blog />
+      <About />
+      <Feedback />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
